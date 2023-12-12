662f963424ca54b66ad15b56de9ea63740825191
sumwas <summayah.waseem@gmail.com>
2023-12-12 10:59:05 -0800

    encyclopedia page

--- lib/src/level_selection/encyclopedia.dart
+++ lib/src/level_selection/encyclopedia.dart
@@ -9,6 +9,43 @@ import '../style/palette.dart';
 class EncyclopediaScreen extends StatelessWidget {
   const EncyclopediaScreen({Key? key}) : super(key: key);
 
+  // Helper method to create a card for each element
+  Widget _buildElementCard(String title, String description, String category, String combination) {
+    final Color cardBackgroundColor = Color.fromRGBO(255, 232, 215, 1.0); // Adjust the alpha value as needed
+    return Card(
+      elevation: 1,
+      margin: const EdgeInsets.only(bottom: 16),
+      color: cardBackgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              description,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Categories: $category',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Combinations: $combination',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final palette = context.watch<Palette>();
@@ -23,7 +60,7 @@ class EncyclopediaScreen extends StatelessWidget {
           Container(
             alignment: Alignment.center,
             color: palette.backgroundSettings,
-            padding: const EdgeInsets.fromLTRB(16, 40, 16, 16), // Adjust top padding
+            padding: const EdgeInsets.fromLTRB(16, 40, 16, 16),
             child: Text(
               'Encyclopedia',
               style: TextStyle(
@@ -33,36 +70,40 @@ class EncyclopediaScreen extends StatelessWidget {
               ),
             ),
           ),
-            Expanded(
-              child: Scrollbar(
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Water: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        SizedBox(height: 16),
-                        Text(
-                          'Earth: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        SizedBox(height: 16),
-                        Text(
-                          'Fire: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        // Add more content as needed
-                      ],
-                    ),
+          Expanded(
+            child: Scrollbar(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildElementCard(
+                        'Water',
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                        'Liquid, Transparent',
+                        'Water + Fire = Steam',
+                      ),
+                      _buildElementCard(
+                        'Earth',
+                        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        'Solid, Brown',
+                        'Earth + Water = Mud',
+                      ),
+                      _buildElementCard(
+                        'Fire',
+                        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                        'Hot, Red',
+                        'Fire + Air = Smoke',
+                      ),
+                      // Add more cards as needed
+                    ],
                   ),
                 ),
               ),
             ),
-            Padding(
+          ),
+          Padding(
             padding: const EdgeInsets.only(bottom: 24.0),
             child: Container(
               height: 60,
@@ -107,8 +148,8 @@ class EncyclopediaScreen extends StatelessWidget {
               ),
             ),
           ),
-          ],
-        ),
-      );
+        ],
+      ),
+    );
   }
 }
